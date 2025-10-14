6d602c0d332c992b6ab5e73637544be90d6eed81
Chad Hart <chad@chadhart.net>
2025-10-14 15:55:33 -0500

    yes

--- utils/test.py
+++ utils/test.py
@@ -1,12 +1,16 @@
 import cv2
+import time
 
 # vid = cv2.VideoCapture('/dev/v4l/by-id/usb-MACROSILICON_USB_Video-video-index0', cv2.CAP_V4L2)
-vid = cv2.VideoCapture('/dev/v4l/by-id/usb-1', cv2.CAP_V4L2)
+vid = cv2.VideoCapture('vid = cv2.VideoCapture('/dev/v4l/by-id/usb-MACROSILICON_USB3._0_capture-video-index0', cv2.CAP_V4L2)', cv2.CAP_V4L2)
+# vid = cv2.VideoCapture('/dev/v4l/by-id/usb-1', cv2.CAP_V4L2)
 
+time.sleep(1)
 vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 vid.set(cv2.CAP_PROP_FPS, 30)
 
+time.sleep(1)
 
 ret, frame = vid.read()
 print(ret)
